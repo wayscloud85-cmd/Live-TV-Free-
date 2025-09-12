d6b8672fe903234af7e9a0d6694751f0b28ea4d5
wayscloud85-cmd <wayscloud85@gmail.com>
2025-09-12 10:20:09 +0500

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,19 @@
+const channelsData=[
+  // Sports
+  {category:"Sports",name:"DD Sports",url:"https://cdn-6.pishow.tv/live/13/master.m3u8",logo:"https://i.imgur.com/J2Ky5OO.png"},
+  {category:"Sports",name:"Star Sports 1",url:"https://muc100.myluck1.top:8088/live/webcricp01/playlist.m3u8",logo:"https://i.imgur.com/E5jjKHI.png"},
+  {category:"Sports",name:"Star Sports 2",url:"https://edge4-moblive.yuppcdn.net/drm1/smil:starsports2drm.smil/chunklist_b996000.m3u8",logo:"https://i.imgur.com/5En7pOI.png"},
+  {category:"Sports",name:"PTV Sports",url:"https://tvsen5.aynaott.com/Ptvsports/tracks-v1a1/mono.ts.m3u8",logo:"https://i.imgur.com/CPm6GHA.png"},
+  {category:"Sports",name:"T Sports",url:"https://tvsen5.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8",logo:"https://i.postimg.cc/7PdvbtGt/T-Sports-logo-svg.png"},
+  
+  // News
+  {category:"News",name:"Geo News",url:"https://jk3lz82elw79-hls-live.5centscdn.com/newgeonews/playlist.m3u8",logo:"https://i.imgur.com/Op4EsaB.png"},
+  {category:"News",name:"ARY News",url:"https://cdn02khi.tamashaweb.com:8087/jazzauth/ARYnews-abr/live/vsat-arynews-L/chunks.m3u8",logo:"https://i.postimg.cc/K85QNzjF/arynews.png"},
+  
+  // Entertainment
+  {category:"Entertainment",name:"Hum Sitaray",url:"https://cdn05khi.tamashaweb.com:8087/jazzauth/HumSitaray-abr/live/vsat-107L/chunks.m3u8",logo:"https://i.postimg.cc/YqTq1Z00/Hum-Sitaray-Logo.png"},
+  {category:"Entertainment",name:"ARY Zindagi",url:"https://cdn23lhr.tamashaweb.com:8087/jazzauth/ARYzindagi-abr/live/vsat-aryzindagi-L/chunks.m3u8",logo:"https://i.postimg.cc/RFMG0PzV/333721437245ccc832fb7b906272d47f-icon.png"},
+  
+  // Documentary
+  {category:"Documentary",name:"Discovery",url:"https://cdn12isb.tamashaweb.com:8087/jazzauth/DiscoveryHD-abr/live/178M/chunks.m3u8",logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2019_Discovery_logo.svg/512px-2019_Discovery_logo.svg.png"}
+];
